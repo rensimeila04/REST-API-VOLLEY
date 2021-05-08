4d4e1ff134e8e23795d6be26194b4f7e29a82531
Rensi <rensimeyla04@gmail.com>
2021-05-08 16:05:55 +0700

    First Commit
    Signed-off-by: Your Name <you@example.com>

---
+++ app/src/main/java/com/rensimyl/www/dotaapi/HeroModel.java
@@ -0,0 +1,33 @@
+package com.rensimyl.www.dotaapi;
+
+public class HeroModel {
+
+
+    private String heroName;
+    private String heroAttr;
+    private String heroAttack;
+    private String heroImg;
+
+    public HeroModel(String heroName, String heroAttr, String heroAttack, String heroImg) {
+        this.heroName = heroName;
+        this.heroAttr = heroAttr;
+        this.heroAttack = heroAttack;
+        this.heroImg = heroImg;
+    }
+
+    public String getHeroName() {
+        return heroName;
+    }
+
+    public String getHeroAttr() {
+        return heroAttr;
+    }
+
+    public String getHeroAttack() {
+        return heroAttack;
+    }
+
+    public String getHeroImg() {
+        return heroImg;
+    }
+}
